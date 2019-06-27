c7a939d9213b8399e58def676608ff2ab8425b5f
Kuncheng <852444214@qq.com>
2019-06-26 23:31:21 -0700

    Finish one page design

---
+++ js/scripts.js
@@ -0,0 +1,64 @@
+
+var ctx_line = document.getElementById('myLineChart');
+var myLineChart = new Chart(ctx_line, {
+    type: 'line',
+    data: {
+        labels: ['12', '13', '14', '15', '16', '17', '18'],
+        datasets: [{
+            label: '微信七日趋势（6.12 - 6.18）',
+            data: [500, 2011, 2690, 3410, 4320, 4671, 5893],
+            backgroundColor: [
+                'rgba(246, 221, 94, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+var ctx_pie1 = document.getElementById("myPieChart1");
+var myPieChart1 = new Chart(ctx_pie1, {
+  type: 'pie',
+  data: {
+    labels: ['裂变式传播', '粉丝转发', '线下活动'],
+    datasets: [{
+      data: [55, 30, 15],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.5)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)'
+      ],
+      borderWidth: 1,
+    }]
+  },
+  options: {
+   	cutoutPercentage: 40,
+    responsive: false,
+  }
+});
