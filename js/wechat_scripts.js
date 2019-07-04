12fb63e12d0bf28d6443ee49b0efb771c5e9dbee
Kuncheng <852444214@qq.com>
2019-07-04 00:06:08 -0700

    添加背景

--- js/wechat_scripts.js
+++ js/wechat_scripts.js
@@ -7,21 +7,13 @@ var myLineChart = new Chart(ctx_line, {
         datasets: [{
             label: '微信七日趋势（6.12 - 6.18）',
             data: [500, 2011, 2690, 3410, 4320, 4671, 5893],
-            backgroundColor: [
-                'rgba(246, 221, 94, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(239, 230, 94, 1)',
+                'rgba(239, 230, 94, 1)',
+                'rgba(239, 230, 94, 1)',
+                'rgba(239, 230, 94, 1)',
+                'rgba(239, 230, 94, 1)',
+                'rgba(239, 230, 94, 1)'
             ],
             borderWidth: 1
         }]
@@ -54,7 +46,7 @@ var myPieChart1 = new Chart(ctx_pie1, {
                 'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)'
             ],
-            borderWidth: 1,
+            borderWidth: 1
         }]
     },
     options: {
